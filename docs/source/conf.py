e15ff7955caa0e7d67608fa9aed070cbbca14824
audrey01 <audrey01@mit.edu>
2024-03-13 17:52:53 -0400

    minor changes to test update of readthedocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,8 +3,8 @@
 # -- Project information
 
 project = 'qnngds'
-# copyright = '2024, Graziella'
-# author = 'Graziella'
+copyright = '2024, QNN group'
+author = 'QNN group'
 
 release = '0.1'
 version = '0.1.0'
