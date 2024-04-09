6595ab512c392918e634cc8ca4d1094ab9773637
Reed Foster <reed.a.foster@gmail.com>
2024-04-09 14:17:24 -0400

    lint

--- src/qnngds/devices.py
+++ src/qnngds/devices.py
@@ -272,60 +272,73 @@ def snspd_vert(
     D.info = S.info
     return D
 
+
 def resistor_meander(
-    layer: int = 1,
     width: float = 2,
     pitch: float = 4,
     squares: float = 100,
     max_length: float = 20,
     aspect_ratio: float = 1,
+    layer: int = 1,
 ) -> Device:
-    """Create resistor meander with specified number of squares
+    """Create resistor meander with specified number of squares.
 
     If squares*width > max_length, meander the resistor, otherwise just return a straight line
-    Args: 
-        layer (int): GDS layer
+    Args:
         width (float): width in microns
         pitch (float): desired pitch of meander in microns
         squares (float): desired number of squares
         max_length (float): desired length of device
         aspect_ratio (float): desired w/h ratio of meander
+        layer (int): GDS layer
 
     Returns:
         D (Device): the resistor meander
     """
-    D = Device('resistor_meander')
+    D = Device("resistor_meander")
 
-    meander_spacing = (pitch - width)/width
+    meander_spacing = (pitch - width) / width
 
-    if width*squares < max_length:
+    if width * squares < max_length:
         # just make a straight
-        return pg.straight(size=(width, width*squares), layer=layer)
+        return pg.straight(size=(width, width * squares), layer=layer)
 
     # make meander
     def hairpin(hp_length):
-        """Create hairpin used in meander"""
-        H = Device('hairpin')
+        """Create hairpin used in meander."""
+        H = Device("hairpin")
         straight = pg.rectangle(size=(hp_length - width, width), layer=layer)
-        conn = pg.rectangle(size=(width, (2 + meander_spacing)*width), layer=layer)
+        conn = pg.rectangle(size=(width, (2 + meander_spacing) * width), layer=layer)
         for i in range(2):
             s = H << straight
-            s.move((-s.xmax, -s.ymin + (1 + meander_spacing)*width*i))
+            s.move((-s.xmax, -s.ymin + (1 + meander_spacing) * width * i))
         c = H << conn
         c.move((-c.xmin, -c.ymin))
         H = pg.union(H, by_layer=True)
-        H.add_port(name=1, midpoint=(-hp_length + width, width/2), width=width, orientation=180)
-        H.add_port(name=2, midpoint=(-hp_length + width, (1 + meander_spacing)*width + width/2), width=width, orientation=180)
+        H.add_port(
+            name=1,
+            midpoint=(-hp_length + width, width / 2),
+            width=width,
+            orientation=180,
+        )
+        H.add_port(
+            name=2,
+            midpoint=(-hp_length + width, (1 + meander_spacing) * width + width / 2),
+            width=width,
+            orientation=180,
+        )
         return H
 
     def stub(orientation):
-        """Create stub to connect to meander ends"""
-        S = Device('stub')
-        straight = pg.rectangle(size=(width, 2*width), layer=layer)
+        """Create stub to connect to meander ends."""
+        S = Device("stub")
+        straight = pg.rectangle(size=(width, 2 * width), layer=layer)
         s = S << straight
         s.move((-s.x, -s.ymin))
-        S.add_port(name=1, midpoint=(0, width/2), width=width, orientation=orientation)
-        S.add_port(name=2, midpoint=(0, 2*width), width=width, orientation=90)
+        S.add_port(
+            name=1, midpoint=(0, width / 2), width=width, orientation=orientation
+        )
+        S.add_port(name=2, midpoint=(0, 2 * width), width=width, orientation=90)
         return S
 
     # solve system for hp_length, n_turn given squares, pitch, aspect_ratio, width:
@@ -335,15 +348,19 @@ def resistor_meander(
     # width_meander/height_meander = aspect_ratio
     #
     # double number of hairpins
-    squares -= 2 # account for extra squares from stubs
-    n_turn = int(2*((16*aspect_ratio*squares*pitch*width+width**2)**0.5 - width)/(8*aspect_ratio*pitch))
-    hp_length = (squares/n_turn-0.5)*width/2
+    squares -= 2  # account for extra squares from stubs
+    n_turn = int(
+        2
+        * ((16 * aspect_ratio * squares * pitch * width + width**2) ** 0.5 - width)
+        / (8 * aspect_ratio * pitch)
+    )
+    hp_length = (squares / n_turn - 0.5) * width / 2
     hp = hairpin(hp_length)
     hp_prev = None
     for i in range(n_turn):
         hp_i = D << hp
         if hp_prev is not None:
-            hp_i.connect(hp_i.ports[2 - (i%2)], hp_prev.ports[2 - (i%2)])
+            hp_i.connect(hp_i.ports[2 - (i % 2)], hp_prev.ports[2 - (i % 2)])
         else:
             stub_top = D << stub(0)
             stub_top.connect(stub_top.ports[1], hp_i.ports[2])
@@ -369,10 +386,10 @@ def resistor_sc_contacts(
     layer_res: int = 2,
     layer_sc: int = 1,
 ) -> Device:
-    """Create resistor meander with superconducting contacts
+    """Create resistor meander with superconducting contacts.
 
     If squares*width > max_length, meander the resistor.
-    Args: 
+    Args:
         width (float): width of resistor
         squares (float): desired number of squares
         max_length (float): desired length of device
@@ -387,13 +404,23 @@ def resistor_sc_contacts(
     Returns:
         D (Device): the resistor meander
     """
-    D = Device('resistor_negtone')
-    aspect_ratio = (contact_width + 2 * outline_sc)/max_length * 1.5
-    res = D << resistor_meander(layer=layer_res, width=width, pitch=max(meander_pitch, width + 1), squares=squares, max_length=max_length, aspect_ratio=aspect_ratio)
+    D = Device("resistor_negtone")
+    aspect_ratio = (contact_width + 2 * outline_sc) / max_length * 1.5
+    res = D << resistor_meander(
+        layer=layer_res,
+        width=width,
+        pitch=max(meander_pitch, width + 1),
+        squares=squares,
+        max_length=max_length,
+        aspect_ratio=aspect_ratio,
+    )
     stub = pg.straight(size=(width, outline_sc), layer=layer_res)
     contact = pg.straight(size=(contact_width, contact_height), layer=layer_res)
-    contact_sc = pg.straight(size=(contact_width + 2*outline_sc, contact_height + 2*outline_sc), layer=layer_sc)
-    rout = pg.straight(size=(width_routing, 2*width_routing), layer=layer_sc)
+    contact_sc = pg.straight(
+        size=(contact_width + 2 * outline_sc, contact_height + 2 * outline_sc),
+        layer=layer_sc,
+    )
+    rout = pg.straight(size=(width_routing, 2 * width_routing), layer=layer_sc)
     ports = []
     for p, port in res.ports.items():
         s = D << stub
@@ -403,11 +430,10 @@ def resistor_sc_contacts(
         c_sc = D << contact_sc
         c_sc.center = c.center
         r = D << rout
-        r.connect(r.ports[1], c_sc.ports[2-(p%2)])
+        r.connect(r.ports[1], c_sc.ports[2 - (p % 2)])
         ports.append(r.ports[2])
     D = pg.union(D, by_layer=True)
     D.add_port(port=ports[0], name=1)
     D.add_port(port=ports[1], name=2)
     D.info = locals()
     return D
-
