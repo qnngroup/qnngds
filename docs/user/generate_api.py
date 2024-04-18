9a868faa54e4e80eb2528a6cd81441921ebe63d7
audrey01 <audrey01@mit.edu>
2024-04-18 10:42:19 -0400

    added a hierarchy in the index page of documentation, and links to the API

--- docs/user/generate_api.py
+++ docs/user/generate_api.py
@@ -28,22 +28,25 @@ def automodule(module, image_folder):
 
 
 def generate_api(src_path):
+
     api = "API\n===\n\n"
 
     modules = [m for m in os.listdir(src_path) if not m.startswith('__')]
     for module in modules:
         if module.endswith('.py'):
-            api += f"{module[:-3].capitalize()}\n"
-            api += f"{'-' * len(module[:-3])}\n\n" 
+            api += f".. _{module[:-3].capitalize()}:\n"  # For referencing this section
+            api += f"{module[:-3].capitalize()}\n"      # The section title
+            api += f"{'-' * len(module[:-3])}\n\n"      # -----------------
             api += automodule(module, image_folder=module[:-3])
         else:
-            api += f"{module.capitalize()}\n"
-            api += f"{'-' * len(module)}\n\n"
+            api += f".. _{module.capitalize()}:\n"  # For referencing this section
+            api += f"{module.capitalize()}\n"      # The section title
+            api += f"{'-' * len(module)}\n\n"      # -----------------
             module_path = os.path.join(src_path, module)
             submodules = [m for m in os.listdir(module_path) if not m.startswith('__') and m.endswith('.py')]
             for submodule in submodules:
-                api += f"{submodule[:-3]}\n"
-                api += f"{'~' * len(submodule[:-3])}\n\n" 
+                api += f"{submodule[:-3]}\n"                # The subsection title
+                api += f"{'~' * len(submodule[:-3])}\n\n"   # ~~~~~~~~~~~~~~~~~~~~
                 api += automodule(f"{module}.{submodule}", image_folder=f"{module}/{submodule[:-3]}")
 
     with open("api.rst", "w") as file:
