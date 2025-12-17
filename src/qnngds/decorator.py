a79f189a288bfd38d92c025f5ac0b14fa2ba55e6
Reed Foster <reed.a.foster@gmail.com>
2025-12-17 11:20:06 -0500

    add some more docs for decorator

--- src/qnngds/decorator.py
+++ src/qnngds/decorator.py
@@ -4,7 +4,39 @@ _devices = {}
 
 
 def device(f):
-    """Decorator to register devices into global variable _devices"""
+    """Decorator to register DeviceFactories into the global variable
+
+    qnngds.decorator._devices.
+
+    For example, if we wish to define a new DeviceFactory that can
+    be used to generate Devices, we can write the following:
+
+    .. code-block:: python
+        :linenos:
+
+        import qnngds as qg
+        from qnngds import Device
+
+        @qg.device
+        def my_new_device(
+            some_param: int,
+            some_other_param: str,
+            layer: LayerSpec,
+            ...
+        ) -> Device:
+            \"\"\"Docstring to explain my function.
+
+            Parameters
+                some_param (int): explanation of some_param.
+                some_other_param (str): explanation of some_other_param.
+                layer (LayerSpec): GDS layer specification.
+                ...
+
+            Returns
+                (Device): description of the device returned
+            \"\"\"
+            ...
+    """
     global _devices
     _devices[f.__name__] = f
     return f
